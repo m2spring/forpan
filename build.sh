fe41c3e07a20785c37a04071e60f05e2a953d14b
Max Spring <mspring@cisco.com>
2025-01-08 16:10:32 -0800

    enhanced build.sh for Jenkins use

--- build.sh
+++ build.sh
@@ -2,8 +2,38 @@
 
 PRJ="$(cd `dirname $0` && pwd)"
 
-cd $PRJ
+nc=${JAVA_HOME:=/opt/jdk21}
+export JAVA_HOME
+export PATH=$JAVA_HOME/bin:$PATH
 
-mvn clean install $@
+nc=${MAVEN_HOME:=/opt/apache-maven-3.9.6}
+export MAVEN_HOME
 
-mvn -f gui javafx:jlink
+if [ -t 1 ]; then 
+    BatchMode=""; 
+else
+    BatchMode="--batch-mode "
+fi
+
+nc=${MVN_OPTIONS:=${BatchMode}--errors --update-snapshots --fail-at-end}
+nc=${MVN_GOAL:=clean install}
+
+nc=${MAVEN_OPTS:=-Xmx512m -Xms512m}
+export MAVEN_OPTS
+
+MvnCmd="${MAVEN_HOME}/bin/mvn"
+
+if [ ! -z "${WORKSPACE}" ]; then
+    MvnRepo="${PRJ}/.repository"
+    mkdir -p "${MvnRepo}"
+    MvnCmd="$MvnCmd -Dmaven.repo.local=${MvnRepo}"
+else
+    cd ${PRJ}
+    export WORKSPACE=${PRJ}
+fi
+
+echo "(export MVN_OPTIONS=\"${MVN_OPTIONS}\"; export MVN_GOAL=\"${MVN_GOAL}\"; ${PRJ}/build.sh)"
+
+set -x
+${MvnCmd} ${MVN_OPTIONS} ${MVN_GOAL} $@
+${MvnCmd} ${MVN_OPTIONS} -f gui javafx:jlink
